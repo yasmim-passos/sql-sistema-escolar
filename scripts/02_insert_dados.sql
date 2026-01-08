0ef8feab9005c29997d3c7d386b203d0501126a5
Yasmim Passos <88737080+yasmim-passos@users.noreply.github.com>
2026-01-08 18:29:16 -0300

    Create 02_insert_dados.sql

---
+++ scripts/02_insert_dados.sql
@@ -0,0 +1,6 @@
+INSERT INTO aluno (nome, data_nascimento, email) VALUES
+('Ana Silva', '2005-03-10', 'ana@email.com'),
+('Carlos Souza', '2004-07-22', 'carlos@email.com');
+
+INSERT INTO professor (nome, email) VALUES
+('Marcos Lima', 'marcos@email.com');
